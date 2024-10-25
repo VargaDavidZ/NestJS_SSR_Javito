a5685f43c9edabb3bc88f47650592647074e7881
VargaDavidZ <varga.zo.david@gmail.com>
2024-10-25 14:03:04 +0200

    .

--- src/holidayForm.dto.ts
+++ src/holidayForm.dto.ts
@@ -3,7 +3,7 @@ export interface HolidayFormDto
     name: string,
     beginDate: string,
     endDate: string,
-    payedTimeOff: boolean,
+    payedTimeOff: string,
     empId: string,
     reason: string // min 30 karakter
 }
